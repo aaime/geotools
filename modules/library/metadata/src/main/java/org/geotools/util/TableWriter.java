45b391c4bfa22d5812951185357f26f8c7dc2473
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2025-06-04 21:37:41 -0300

    Fix errorprone in gt-metadata

--- modules/library/metadata/src/main/java/org/geotools/util/TableWriter.java
+++ modules/library/metadata/src/main/java/org/geotools/util/TableWriter.java
@@ -757,7 +757,7 @@ public class TableWriter extends FilterWriter {
              * alors cette cellule devra s'écrire sur plusieurs lignes dans la cellule
              * courante.
              */
-            while (!isEmpty(currentLine)) {
+            while (!isEmpty((Object[]) currentLine)) {
                 for (int j = 0; j < currentLine.length; j++) {
                     final boolean isFirstColumn = (j == 0);
                     final boolean isLastColumn = (j + 1 == currentLine.length);
